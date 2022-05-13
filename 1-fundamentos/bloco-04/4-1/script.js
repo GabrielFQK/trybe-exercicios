e6e4e704fb6b02653e966798b852bdd5b1dfc613
Gabriel Ferreira <gabrielfqk@gmail.com>
2022-05-12 21:50:13 -0400

    adicionado parte 11 do exercício do dia 4-1 do módulo de fundamentos

--- 1-fundamentos/bloco-04/4-1/script.js
+++ 1-fundamentos/bloco-04/4-1/script.js
@@ -151,3 +151,37 @@ if (precoCusto < 0 || precoVenda < 0) {
   console.log('Lucro ao vender 1000 peças: ' + lucro * 1000)
 }
 
+console.log('================')
+console.log('Exercício 11')
+
+const salarioBruto = 3000
+let inss
+
+if (salarioBruto <= 1556.94) {
+  inss = salarioBruto * 0.8
+} else if (salarioBruto <= 2594.92) {
+  inss = salarioBruto * 0.9
+} else if (salarioBruto <= 5189.82) {
+  inss = salarioBruto * 0.11
+} else if (salarioBruto > 5189.82) {
+  inss = 570.88
+}
+
+const salarioBase = salarioBruto - inss
+let ir
+
+if (salarioBase <= 1903.98) {
+  ir = 0
+} else if (salarioBase <= 2826.65) {
+  ir = salarioBase * 0.075 - 142.8
+} else if (salarioBase <= 3751.05) {
+  ir = salarioBase * 0.015 - 354.8
+} else if (salarioBase <= 4664.68) {
+  ir = salarioBase * 0.225 - 636.13
+} else if (salarioBase > 4664.68) {
+  ir = salarioBase * 0.275 - 869.36
+}
+
+const salarioLiquido = salarioBase - ir
+
+console.log(salarioLiquido)
