9d348eced40f9accb4f34d7d43ad12e9b8313481
Gabriel Ferreira <gabrielfqk@gmail.com>
2022-05-12 21:47:29 -0400

    adicionado parte 1 do exercício do dia 4-1 do módulo de fundamentos

---
+++ 1-fundamentos/bloco-04/4-1/script.js
@@ -0,0 +1,12 @@
+console.log('Exercício 1')
+
+const a = 10
+const b = 5
+
+const adicao = a + b
+const subtracao = a - b
+const multiplicacao = a * b
+const divisao = a / b
+const modulo = a % b
+
+console.log(adicao, subtracao, multiplicacao, divisao, modulo)
