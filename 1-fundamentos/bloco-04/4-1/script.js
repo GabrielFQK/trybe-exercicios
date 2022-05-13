ef5b4d6176dc65b5ec5b51a342d113336305b573
Gabriel Ferreira <gabrielfqk@gmail.com>
2022-05-12 21:47:55 -0400

    adicionado parte 2 do exercício do dia 4-1 do módulo de fundamentos

--- 1-fundamentos/bloco-04/4-1/script.js
+++ 1-fundamentos/bloco-04/4-1/script.js
@@ -10,3 +10,15 @@ const divisao = a / b
 const modulo = a % b
 
 console.log(adicao, subtracao, multiplicacao, divisao, modulo)
+
+console.log('================')
+console.log('Exercício 2')
+
+const num1 = 4
+const num2 = 7
+
+if (num1 > num2) {
+  console.log('O primeiro número é maior')
+} else {
+  console.log('O segundo número é maior')
+}
