d879f4460908a04ecb6f55475255128172a15cdd
Gabriel Ferreira <gabrielfqk@gmail.com>
2022-05-17 17:59:46 -0400

    :bricks: Modificado script.js do bloco 4-4 - Adicionado exercício 5

--- 1-fundamentos/bloco-04/4-4/script.js
+++ 1-fundamentos/bloco-04/4-4/script.js
@@ -33,3 +33,25 @@ console.log('================')
 for (let chave in info) {
   console.log(info[chave])
 }
+
+console.log('================')
+console.log('Exercício 5')
+console.log('================')
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+console.log(`${info.personagem} e ${info2.personagem}`)
+console.log(`${info.origem} e ${info2.origem}`)
+console.log(`${info.nota} e ${info2.nota}`)
+if (info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+  console.log('Ambos recorrentes')
+} else if (info.recorrente === 'Não' && info2.recorrente === 'Não') {
+  console.log('Nenhum recorrente')
+} else {
+  console.log(`${info.recorrente} e ${info2.recorrente}`)
+}
