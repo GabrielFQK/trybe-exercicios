ade7387a9cefe7fa1ed627d5476e1adc024c19b9
Gabriel Ferreira <gabrielfqk@gmail.com>
2022-05-17 17:41:21 -0400

    :bricks: Modificado script.js do bloco 4-4 - Adicionado exercício 1

---
+++ 1-fundamentos/bloco-04/4-4/script.js
@@ -0,0 +1,11 @@
+console.log('================')
+console.log('Exercício 1')
+console.log('================')
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald'
+}
+
+console.log(`Bem-vinda, ${info.personagem}!`)
