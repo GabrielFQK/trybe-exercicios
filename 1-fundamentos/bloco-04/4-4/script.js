3a208bb6b366dfa41ad27a4147118eb254815263
Gabriel Ferreira <gabrielfqk@gmail.com>
2022-05-17 18:02:50 -0400

    :bricks: Modificado script.js do bloco 4-4 - Adicionado exercício 6

--- 1-fundamentos/bloco-04/4-4/script.js
+++ 1-fundamentos/bloco-04/4-4/script.js
@@ -55,3 +55,22 @@ if (info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
 } else {
   console.log(`${info.recorrente} e ${info2.recorrente}`)
 }
+
+console.log('================')
+console.log('Exercício 6')
+console.log('================')
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas'
+    }
+  ]
+}
+
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`)
